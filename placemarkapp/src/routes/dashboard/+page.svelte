5307c57bfcea7c142fc3b9340d3aafeb2b4947e0
Frank Donnellan <97000920+frankied67@users.noreply.github.com>
2024-05-23 00:52:24 +0100

    previous dashbord restored

--- placemarkapp/src/routes/dashboard/+page.svelte
+++ placemarkapp/src/routes/dashboard/+page.svelte
@@ -1,25 +1,15 @@
 <script lang="ts">
-  import { locationService } from "$lib/services/location-service";
-  import { Location } from "$lib/services/location-types";
-  import { currentSession, subTitle } from "$lib/stores";
+  import { subTitle } from "$lib/stores";
   import DashboardForm from "./DashboardForm.svelte";
   import ListLocations from "$lib/ui/ListLocations.svelte";
   import Card from "$lib/ui/Card.svelte";
-  import { onMount } from "svelte";
-  import { get } from "svelte/store";
-
-  let locations: Location[] = [];
-
-  onMount(async () => {
-    locations = await locationService.getLocations(get(currentSession));
-  });
-
   subTitle.set("Add a Location");
 </script>
 
+<DashboardForm />
 <Card title="Please add a location">
   <DashboardForm />
 </Card>
-<Card title="Locations">
-  <ListLocations {locations} />
+<Card title="Location">
+  <ListLocations />
 </Card>
