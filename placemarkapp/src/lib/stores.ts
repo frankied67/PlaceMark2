211533390265f81afefd6634410f0da4b9e496d4
Frank Donnellan <97000920+frankied67@users.noreply.github.com>
2024-05-23 00:36:56 +0100

    Types folder added, services folder added, dashboard page updated

--- placemarkapp/src/lib/stores.ts
+++ placemarkapp/src/lib/stores.ts
@@ -1,5 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
 import { writable } from "svelte/store";
+import type { Session } from "$lib/types/location-types";
 
 export const subTitle = writable<string>();
-export const currentSession = writable<string>();
+export const currentSession = writable<Session>();
--- placemarkapp/src/routes/dashboard/+page.svelte
+++ placemarkapp/src/routes/dashboard/+page.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
-  import { subTitle } from "$lib/stores";
+  import { locationService } from "$lib/services/location-service";
+  import { Location } from "$lib/services/location-types";
+  import { currentSession, subTitle } from "$lib/stores";
   import DashboardForm from "./DashboardForm.svelte";
   import ListLocations from "$lib/ui/ListLocations.svelte";
   import Card from "$lib/ui/Card.svelte";
+  import { onMount } from "svelte";
+  import { get } from "svelte/store";
+
+  let locations: Location[] = [];
+
+  onMount(async () => {
+    locations = await locationService.getLocations(get(currentSession));
+  });
 
   subTitle.set("Add a Location");
 </script>
@@ -10,6 +20,6 @@
 <Card title="Please add a location">
   <DashboardForm />
 </Card>
-<Card title="Location">
-  <ListLocations />
+<Card title="Locations">
+  <ListLocations {locations} />
 </Card>
