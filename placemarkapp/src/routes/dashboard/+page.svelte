441008a11c84be9b8d3ce53a5b6dfca13ce44ecd
Frank Donnellan <97000920+frankied67@users.noreply.github.com>
2024-05-26 00:29:58 +0100

    Revert "previous dashbord restored"
    This reverts commit 5307c57bfcea7c142fc3b9340d3aafeb2b4947e0.

--- placemarkapp/src/routes/dashboard/+page.svelte
+++ placemarkapp/src/routes/dashboard/+page.svelte
@@ -1,16 +1,24 @@
 <script lang="ts">
+  import { locationService } from "$lib/services/location-service";
+  import { Location } from "$lib/services/location-types";
   import { currentSession, subTitle } from "$lib/stores";
   import DashboardForm from "./DashboardForm.svelte";
   import Card from "$lib/ui/Card.svelte";
-  import { monumentService } from "$lib/services/monument-service";
   import { onMount } from "svelte";
   import { get } from "svelte/store";
-  import type { Monument } from "$lib/types/monument-types";
 
-  //let monumentList: Monument[] = [];
-  subTitle.set("Add a Monument");
+  let locations: Location[] = [];
+
+  onMount(async () => {
+    locations = await locationService.getLocations(get(currentSession));
+  });
+
+  subTitle.set("Add a Location");
 </script>
 
-<Card title="Please add a Monument">
+<Card title="Please add a location">
   <DashboardForm />
+</Card>
+<Card title="Locations">
+  <ListLocations {locations} />
 </Card>
